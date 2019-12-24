92fd3a8296d7f3c6496f62c95bdfe935734cbc5e
Byron Vanegas <designerbyron@gmail.com>
2019-12-23 23:10:25 -0600

    Estructura inicial del proyecto lista junto a todas las librerias

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+    <!-- Bootstrap 4.4.1 -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="plugins/sweetalert2/sweetalert2.miin.css">
+    
+</head>
+<body>
+
+
+
+
+
+
+    <script src="jquery/jquery-3.4.1.min.js"></script>
+
+    <script src="popper/popper.min.js"></script>
+
+    <script src="bootstrap/bootstrap.min.js"></script>
+    <!-- Bootstrap.js 4.4.1 -->
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <!-- Enlace CDN de vue.js -->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.0/axios.min.js"></script>
+    <!-- Enlace axios para peticiones ajax  -->
+
+    <script src="plugins/sweetalert2.all.min.js"></script>
+    <!-- Sweet Alert para mensajes mas amigables y faciles  -->
+    <script src="main.js"></script>
+
+</body>
+</html>
